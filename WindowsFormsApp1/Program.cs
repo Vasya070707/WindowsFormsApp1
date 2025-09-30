d22bfa5df37b2d72dff01d383ae01634998e6b05
Slava <Slava@DESKTOP-L9441NQ>
2025-09-30 22:46:59 +0500

    dskjfsda

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -12,6 +12,7 @@ namespace ТекстовыйРедактор
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("RADMI DSA");
         }
     }
 }
